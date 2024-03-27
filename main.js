677b6b1576b241bd2737714590eb1338ec7e6f4d
Raguf <97394996+Raguf@users.noreply.github.com>
2024-03-27 23:08:32 +0400

    Switching the Active Player

--- main.js
+++ main.js
@@ -2,20 +2,24 @@
 
 //---------- Selecting elements ----------
 const diceElement = document.querySelector('.dice');
+const player0Element = document.querySelector('.player--0');
+const player1Element = document.querySelector('.player--1');
 const score0Element = document.querySelector('#score--0');
 const score1Element = document.querySelector('#score--1');
 const current0Element = document.querySelector('#current--0');
 const current1Element = document.querySelector('#current--1');
-const btnNew = document.querySelector('.btn--new');
+
+const btnNew = document.querySelector('.bnt--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
-
 //---------- Selecting elements ----------
 score0Element.textContent = 0;
 score1Element.textContent = 0;
 diceElement.classList.add('hidden');
 
 let currentScore = 0;
+let activePlayer = 0;
+
 // Rolling functionality for dice
 btnRoll.addEventListener('click', function () {
   const dice = Math.trunc(Math.random() * 6) + 1;
@@ -24,14 +28,18 @@ btnRoll.addEventListener('click', function () {
   // Display dices
   diceElement.classList.remove('hidden');
   diceElement.src = `dice-${dice}.png`;
-
   // Check for rolled: if is true
   if (dice !== 1) {
     // Add dice to current score
     currentScore += dice;
-    current0Element.textContent = currentScore;
-  }
-  // Else switch to other player
-  else {
+    document.querySelector(`#current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // Else switch to other player
+    document.querySelector(`#current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    currentScore = 0;
+    player0Element.classList.toggle('player--active');
+    player1Element.classList.toggle('player--active');
   }
 });
